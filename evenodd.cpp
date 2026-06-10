432750e6d5d7cb2b6489873cb328599849cf4d7d
yrajdixit <yrajdixit@gmail.com>
2026-06-10 23:22:44 +0530

    inital commit

---
+++ evenodd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long N;
+    cin >> N;
+
+    if (N % 2 == 0)
+        cout << "Even" << endl;
+    else
+        cout << "Odd" << endl;
+
+    return 0;
+}
